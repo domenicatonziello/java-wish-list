4cc5dd52d9a986ad8646d0a5e1a57e1ebdf7feb9
domenicatonziello <domenica.tonziello10@gmail.com>
2023-05-12 15:18:55 +0200

    MAP bonus

--- src/org/java/mymap/Main.java
+++ src/org/java/mymap/Main.java
@@ -2,7 +2,6 @@ package org.java.mymap;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -12,14 +11,23 @@ public class Main {
 
 		System.out.println(Arrays.asList(str.split(" ")));
 		
+		
+		String[] arr = str.split(" ");
+		
 		Map<String, Integer> myMap = new HashMap<>();
 		
-		for (String word : str.split(" ") ) {
+		for (String word : arr ) {
+			
+			word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 			
 			if (myMap.containsKey(word)) {
 				myMap.put(word, myMap.get(word) + 1);
 			} else {
 				myMap.put(word, 1);
+			}
+			
+			if(word.length() == 0 ) {
+				myMap.remove(word);
 			}
 		}
 		
